8296643c1783fc727c0823fa16ff1e0b581a9bdd
Ted Pioli <ejpioli@massart.edu>
2015-11-21 15:09:26 -0500

    A few edits

---
+++ instagram/main.js
@@ -0,0 +1,17 @@
+var images= null;
+function drawImages(data) { console.log(data)
+images= data;
+
+for(var i=0;i<images.data.length;i++) {
+  addImage(images.data[i]);
+  }
+}
+
+ function addImage(item) {
+   var url= item.images.thumbnail.url;
+   var $container = $(".container");
+
+   var $img = $("<img>").attr("src",url);
+   
+   $container.append($img);
+}
